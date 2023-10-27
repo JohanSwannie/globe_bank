a56b5059457f427edfe80138fc1003a3fc399c83
Johan Swan <jswannieman@gmail.com>
2023-10-27 16:25:24 +1300

    Commit db_connect_guide PHP Component

--- db_connect_guide.php
+++ db_connect_guide.php
@@ -4,12 +4,14 @@
 // of database interaction using PHP.
 
 // Credentials
+
 $dbhost = '';
 $dbuser = '';
 $dbpass = '';
 $dbname = '';
 
 // 1. Create a database connection
+
 $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 // 2. Perform database query
@@ -19,6 +21,7 @@ $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 // 4. Release returned data
 
 // 5. Close database connection
+
 mysqli_close($connection);
 
 ?>
