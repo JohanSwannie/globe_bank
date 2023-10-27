f563f35505e506e25db0fa6aa43d9938969f0fc6
Johan Swan <jswannieman@gmail.com>
2023-10-28 12:41:46 +1300

    Commit db_credentials PHP Component

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -6,3 +6,10 @@ define("DB_PASS", "gpimbv#oak&l777");
 define("DB_NAME", "globe_bank");
 
 ?>
+
+<!-- How to set up the above on the command line 
+
+CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
+
+GRANT ALL PRIVILEGES ON db_name.* TO 'username'@'localhost'; -->
+
