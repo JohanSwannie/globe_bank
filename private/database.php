842d11085ec9e29b2a24a0bee8e38211097f8018
Johan Swan <jswannieman@gmail.com>
2023-10-29 11:23:29 +1300

    Commit database PHP Component

--- private/database.php
+++ private/database.php
@@ -15,7 +15,7 @@
   }
 
   function confirm_db_connect() {
-    if(mysqli_connect_error()) {
+    if(mysqli_connect_errno()) {
       $msg = "Database connection failed: ";
       $msg .= mysqli_connect_error();
       $msg .= " (" . mysqli_connect_errno() . ")";
