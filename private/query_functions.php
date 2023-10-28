21d7ddeaafa787c8941725e368240fe31057eda0
Johan Swan <jswannieman@gmail.com>
2023-10-28 15:33:02 +1300

    Update query_functions PHP Component

--- private/query_functions.php
+++ private/query_functions.php
@@ -4,10 +4,14 @@
     global $db;
 
     $sql = "SELECT * FROM subjects ORDER BY position ASC";
+
     // OR
     // $sql = "SELECT * FROM subjects";
     // $sql .= " ORDER BY position ASC";
-    // Testing sql by typing "echo $sql";
+
+    // Testing sql
+    // echo $sql;
+    
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
     return $result;
