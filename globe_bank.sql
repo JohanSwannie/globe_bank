936900129d9c44a351331ef5532af72319691d9f
Johan Swan <jswannieman@gmail.com>
2023-10-29 12:37:20 +1300

    Update globe_bank.sql

--- globe_bank.sql
+++ globe_bank.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS `pages`;
+
 CREATE TABLE `pages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `subject_id` int(11) DEFAULT NULL,
@@ -13,6 +14,7 @@ CREATE TABLE `pages` (
 INSERT INTO `pages` VALUES (1,1,'Globe Bank',1,1,NULL),(2,1,'History',2,1,NULL),(3,1,'Leadership',3,1,NULL),(4,1,'Contact Us',4,1,NULL),(5,2,'Banking',1,1,NULL),(6,2,'Credit Cards',2,1,NULL),(7,2,'Mortgages',3,1,NULL),(8,3,'Checking',1,1,NULL),(9,3,'Loans',2,1,NULL),(10,3,'Merchant Services',3,1,NULL);
 
 DROP TABLE IF EXISTS `subjects`;
+
 CREATE TABLE `subjects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `menu_name` varchar(255) DEFAULT NULL,
