cb7230ed71a149795092e56f598a9bb7dd2c980f
Johan Swan <jswannieman@gmail.com>
2023-10-30 11:46:43 +1300

    Update sql_operations_functions PHP Component

--- private/sql_operations_functions.php
+++ private/sql_operations_functions.php
@@ -1,17 +1,13 @@
 <?php
 
+  // Subjects
+
   function find_all_subjects() {
     global $db;
 
-    $sql = "SELECT * FROM subjects ORDER BY position ASC";
-
-    // OR
-    // $sql = "SELECT * FROM subjects";
-    // $sql .= " ORDER BY position ASC";
-
-    // Testing sql
-    // echo $sql;
-    
+    $sql = "SELECT * FROM subjects ";
+    $sql .= "ORDER BY position ASC";
+    //echo $sql;
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
     return $result;
@@ -29,15 +25,15 @@
     return $subject; // returns an assoc. array
   }
 
-  function insert_subject($menu_name, $position, $visible) {
+  function insert_subject($subject) {
     global $db;
 
     $sql = "INSERT INTO subjects ";
     $sql .= "(menu_name, position, visible) ";
     $sql .= "VALUES (";
-    $sql .= "'" . $menu_name . "',";
-    $sql .= "'" . $position . "',";
-    $sql .= "'" . $visible . "'";
+    $sql .= "'" . $subject['menu_name'] . "',";
+    $sql .= "'" . $subject['position'] . "',";
+    $sql .= "'" . $subject['visible'] . "'";
     $sql .= ")";
     $result = mysqli_query($db, $sql);
     // For INSERT statements, $result is true/false
@@ -74,6 +70,27 @@
 
   }
 
+  function delete_subject($id) {
+    global $db;
+
+    $sql = "DELETE FROM subjects ";
+    $sql .= "WHERE id='" . $id . "' ";
+    $sql .= "LIMIT 1";
+    $result = mysqli_query($db, $sql);
+
+    // For DELETE statements, $result is true/false
+    if($result) {
+      return true;
+    } else {
+      // DELETE failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
+    }
+  }
+
+  // Pages
+
   function find_all_pages() {
     global $db;
 
@@ -82,6 +99,86 @@
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
     return $result;
+  }
+
+  function find_page_by_id($id) {
+    global $db;
+
+    $sql = "SELECT * FROM pages ";
+    $sql .= "WHERE id='" . $id . "'";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    $page = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    return $page; // returns an assoc. array
+  }
+
+  function insert_page($page) {
+    global $db;
+
+    $sql = "INSERT INTO pages ";
+    $sql .= "(subject_id, menu_name, position, visible, content) ";
+    $sql .= "VALUES (";
+    $sql .= "'" . $page['subject_id'] . "',";
+    $sql .= "'" . $page['menu_name'] . "',";
+    $sql .= "'" . $page['position'] . "',";
+    $sql .= "'" . $page['visible'] . "',";
+    $sql .= "'" . $page['content'] . "'";
+    $sql .= ")";
+    $result = mysqli_query($db, $sql);
+    // For INSERT statements, $result is true/false
+    if($result) {
+      return true;
+    } else {
+      // INSERT failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
+    }
+  }
+
+  function update_page($page) {
+    global $db;
+
+    $sql = "UPDATE pages SET ";
+    $sql .= "subject_id='" . $page['subject_id'] . "', ";
+    $sql .= "menu_name='" . $page['menu_name'] . "', ";
+    $sql .= "position='" . $page['position'] . "', ";
+    $sql .= "visible='" . $page['visible'] . "', ";
+    $sql .= "content='" . $page['content'] . "' ";
+    $sql .= "WHERE id='" . $page['id'] . "' ";
+    $sql .= "LIMIT 1";
+
+    $result = mysqli_query($db, $sql);
+    // For UPDATE statements, $result is true/false
+    if($result) {
+      return true;
+    } else {
+      // UPDATE failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
+    }
+
+  }
+
+  function delete_page($id) {
+    global $db;
+
+    $sql = "DELETE FROM pages ";
+    $sql .= "WHERE id='" . $id . "' ";
+    $sql .= "LIMIT 1";
+    $result = mysqli_query($db, $sql);
+
+    // For DELETE statements, $result is true/false
+    if($result) {
+      return true;
+    } else {
+      // DELETE failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
+    }
   }
 
 ?>
