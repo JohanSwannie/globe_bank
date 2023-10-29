cda1782d91d341e0093fc0ad08ba2e28b42c00f3
Johan Swan <jswannieman@gmail.com>
2023-10-30 09:28:22 +1300

    Update sql_operations_functions PHP Component

---
+++ private/sql_operations_functions.php
@@ -0,0 +1,43 @@
+<?php
+
+  function find_all_subjects() {
+    global $db;
+
+    $sql = "SELECT * FROM subjects ORDER BY position ASC";
+
+    // OR
+    // $sql = "SELECT * FROM subjects";
+    // $sql .= " ORDER BY position ASC";
+
+    // Testing sql
+    // echo $sql;
+    
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    return $result;
+  }
+
+  function find_subject_by_id($id) {
+    global $db;
+
+    $sql = "SELECT * FROM subjects ";
+    $sql .= "WHERE id='" . $id . "'";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    $subject = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    return $subject; // returns an assoc. array
+  }
+
+
+  function find_all_pages() {
+    global $db;
+
+    $sql = "SELECT * FROM pages ";
+    $sql .= "ORDER BY subject_id ASC, position ASC";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    return $result;
+  }
+
+?>
