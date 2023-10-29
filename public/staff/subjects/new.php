aa3506acdfb9db294bff62933baa9915373be635
Johan Swan <jswannieman@gmail.com>
2023-10-30 09:33:57 +1300

    Update subjects new PHP Component

--- public/staff/subjects/new.php
+++ public/staff/subjects/new.php
@@ -2,20 +2,9 @@
 
 require_once('../../../private/initialize.php');
 
-$test = $_GET['test'] ?? '';
-
-if($test == '404') {
-  error_404();
-} elseif($test == '500') {
-  error_500();
-} elseif($test == 'redirect') {
-  redirect_to(url_for('/staff/subjects/index.php'));
-}
-
 ?>
 
 <?php $page_title = 'Create Subject'; ?>
-
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
